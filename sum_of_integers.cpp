954125f04aa1e499612e4381896f9b2470422950
Emmanuel Fofeyin Ngwamunwi <emmanuel.fofeyin-ngwamunwi@mths.ca>
2022-12-13 15:47:10 +0000

    done

--- sum_of_integers.cpp
+++ sum_of_integers.cpp
@@ -8,34 +8,35 @@
 #include <string>
 
 // creating variables
-int inputNumber;
-int positiveInteger int 
+int inputNumberInt;
+int positiveInteger;
 int repeat = 0;
 int positiveAnswer = 0;
 int negativeAnswer = 0;
+int inputNumber;
 
 int main() {
     // This program calculates the positive an negative integer sum
 
     //  Input,Process and output
-
     try {
-        while (repeat) < 4(repeat = repeat + 1)
-        inputNumber = std::cout << "Enter an integer ";
-        inputNumberInt = std::stoi(inputNumber);
+        while (repeat < 4) {
+            (repeat = repeat + 1);
+            std::cout << "Enter an integer ";
+            std::cin >> inputNumber;
+            inputNumberInt = (inputNumber);
+        }
         if (inputNumber >= 0) {
-            (positiveAnswer) = (positiveAnswer) + (inputNumberInt);
+            (positiveAnswer) = (positiveAnswer + inputNumberInt);
             if (inputNumber < 0) {
                 (negativeAnswer) = (negativeAnswer) + (inputNumberInt);
-                std::cout << This is invalid input << std::endl;
+                std::cout << "This is invalid input" << std::endl;
             } else {
-                std::cout << "The negative sum is" << negativeAnswer << "
-                    the positive sum is " << positiveAnswer <<;
+                std::cout << "The negative sum is" << negativeAnswer << "the positive sum is " << positiveAnswer;
             }
         }
-        catch (std::invalid_argument) {
-            std::cout << This is invalid input << std::endl;
-        }
+    } catch (std::invalid_argument) {
+        std::cout << "This is invalid input" << std::endl;
 
         std::cout << "\nDone.";
     }
